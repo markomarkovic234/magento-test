f3ed0f916f2f149c66cf6553e030a1622ed5eccb
markomarkovic234 <mdmbgd@gmail.com>
2025-07-16 17:22:14 +0200

    Add files via upload

--- magento.py
+++ magento.py
@@ -20,5 +20,23 @@ def test_magento_smoke():
     driver = open_site('https://magento.softwaretestingboard.com/')
     sleep(2)
     assert "home" in driver.title.lower() or "page" in driver.title.lower(), "Title check failed"
+    assert driver.current_url == "https://magento.softwaretestingboard.com/" or "magento" in driver.current_url
     print('Home page title verified.')
+    close_browser(driver)
+
+def test_luma_logo_redirects_to_homepage():
+    driver = open_site('https://magento.softwaretestingboard.com/')
+    sleep(2)
+    consent_button = driver.find_element(By.XPATH, "//p[text()='Consent']")
+    consent_button.click()
+    sleep(2)
+    # Click the "Shop New Yoga" link
+    element = driver.find_element(By.XPATH, "//a[@class='block-promo home-main']//span[text()='Shop New Yoga']")
+    element.click()
+    # Click on the logo
+    logo = driver.find_element(By.CSS_SELECTOR, "a.logo")
+    logo.click()
+    sleep(3)
+    assert driver.current_url == "https://magento.softwaretestingboard.com/" or "magento" in driver.current_url
+    print("Test passed: Luma logo redirected to homepage")
     close_browser(driver)
